c583f910dee50caa5817404b3b910d71ba9d56f5
Edwin Croes <ed_croes@hotmail.com>
2023-12-22 10:38:31 +0100

    Day 22, Small refactoring

--- AoC2023/Day22/Day22.cs
+++ AoC2023/Day22/Day22.cs
@@ -11,7 +11,7 @@ public class Day22 : IMDay
     {
         var lines = await GetInput();
         if (_supporting is null || _supportedBy is null)
-            (_supporting, _supportedBy) = GetFallenStack(lines);
+            (_supporting, _supportedBy) = GetStack(lines);
 
         return _supportedBy.Keys.Count(k => _supportedBy.Keys.Where(o => _supportedBy[o].Contains(k)).All(o => _supportedBy[o].Length > 1)).ToString();
     }
@@ -20,7 +20,7 @@ public class Day22 : IMDay
     {
         var cuboids = await GetInput();
         if (_supporting is null || _supportedBy is null)
-            (_supporting, _supportedBy) = GetFallenStack(cuboids);
+            (_supporting, _supportedBy) = GetStack(cuboids);
 
         var totalFallen = 0;
         foreach (var brick in _supportedBy.Keys)
@@ -45,15 +45,15 @@ public class Day22 : IMDay
             LetBrickFall(next, fallen);
     }
 
-    private static (Dictionary<Line3D, List<Line3D>> supporting, Dictionary<Line3D, Line3D[]> supportedBy) GetFallenStack(IEnumerable<Line3D> lines)
+    private static (Dictionary<Line3D, List<Line3D>> supporting, Dictionary<Line3D, Line3D[]> supportedBy) GetStack(IEnumerable<Line3D> lines)
     {
-        List<Line3D> fallen = [new(new(-1, -1, -1), new(-1, -1, -1))];
+        HashSet<Line3D> fallen = [new(new(-1, -1, -1), new(-1, -1, -1))]; // Add dummy so that Max() won't complain
         Dictionary<Line3D, List<Line3D>> supporting = [];
         Dictionary<Line3D, Line3D[]> supportedBy = [];
 
         foreach (var line in lines.OrderBy(c => c.From.Z))
         {
-            var newZ = 1 + fallen.Max(l => (l.HasOverlapOnXAndYWith(line)) ? l.To.Z : 0);
+            var newZ = 1 + fallen.Max(l => l.HasOverlapOnXAndYWith(line) ? l.To.Z : 0);
             Line3D newLine = new(new(line.From.X, line.From.Y, newZ), new(line.To.X, line.To.Y, newZ + (line.To.Z - line.From.Z)));
 
             var lower = fallen
